36689c634a6240fa8f3416adf72c75d7cf7c11ed
Cody Choi <cchoi_2294@hotmail.com>
2020-11-26 01:13:17 -0500

    tweaked height and responsive height for therapy gif

--- src/app/components/commons/gifContainer/gifContainer.tsx
+++ src/app/components/commons/gifContainer/gifContainer.tsx
@@ -4,15 +4,13 @@ import cls from './gifContainer.module.scss';
 
 interface GifAttributes {
   link?: string;
-  sizeRatio?: number;
 }
 
 export const GifContainer: React.FC<GifAttributes> = ({
   link = 'https://streamable.com/e/i2wph9?autoplay=1',
-  sizeRatio = 35,
 }) => {
   return (
-    <div className={cls.wrapper} style={{ paddingBottom: `${sizeRatio}%` }}>
+    <div className={cls.wrapper}>
       <iframe
         title='gifIframe'
         className={cls.media}
