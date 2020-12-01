1f00e3cab78439257ac83fae38520929503a6b0e
Cody Choi <cchoi_2294@hotmail.com>
2020-12-01 14:14:46 -0500

    updated copy for therapy side description page

--- src/app/components/therapy/therapy.tsx
+++ src/app/components/therapy/therapy.tsx
@@ -4,6 +4,9 @@ import { GifContainer } from 'app/components/commons/gifContainer/gifContainer';
 import classNames from 'classnames';
 import { Path } from 'app/Path';
 import { SideDescription } from 'app/components/commons/sideDescription/sideDescription';
+import SupportedFingerTech from 'app/assets/images/handTechniques/supportedFingerTech.svg';
+import SupportedHandTech from 'app/assets/images/handTechniques/supportedHandTech.svg';
+import SupportedThumbTech from 'app/assets/images/handTechniques/supportedThumbTech.svg';
 
 import cls from './therapy.module.scss';
 
@@ -22,52 +25,49 @@ export const Therapy: React.FC = () => {
           </a>
           <p className={cls.header}>Try these out.</p>
           <GifContainer link={selectedExercise.gif} />
-          <div className={cls.excercisesContainer}>
-            {exerciseList.map(({ name, thumbnail, gif }, index) => (
-              <button
-                key={`${name}${index}`}
-                className={classNames('btn', cls.thumbnailContainer, {
-                  [cls.activeThumbnail]: selectedExercise.name === name,
-                })}
-                onClick={() =>
-                  setSelectedExercise({
-                    name,
-                    thumbnail,
-                    gif,
-                  })
-                }
-              >
-                <img
-                  className={cls.thumbnail}
-                  src={thumbnail}
-                  alt={`thumbnail img of ${name} exercise`}
-                />
-              </button>
-            ))}
+          <div className={cls.excercisesWrapper}>
+            <p className={'bold'}>Techniques to try:</p>
+            <div className={cls.excercisesContainer}>
+              {exerciseList.map(({ name, thumbnail, gif }, index) => (
+                <button
+                  key={`${name}${index}`}
+                  className={classNames(cls.thumbnailContainer, {
+                    [cls.activeThumbnail]: selectedExercise.name === name,
+                  })}
+                  onClick={() =>
+                    setSelectedExercise({
+                      name,
+                      thumbnail,
+                      gif,
+                    })
+                  }
+                >
+                  <img
+                    className={cls.thumbnail}
+                    src={thumbnail}
+                    alt={`thumbnail img of ${name} exercise`}
+                  />
+                </button>
+              ))}
+            </div>
           </div>
         </div>
         <SideDescription toolTipTitle={'Message Tips & Tricks'}>
           <div className={'infoBox'}>
             <div className={'title'}>
-              <p>Message Techniques</p>
+              <p>Massage Guidlines</p>
             </div>
             <div className={'description'}>
-              <p>
-                This is a library of massage techniques based on the teachings
-                from Clair Davies{' '}
-                <span className={'bold'}>
-                  “Trigger Point Therapy Work Book”.
-                </span>{' '}
-                Now you can make your own pain go away without relying on
-                anybody.
-              </p>
-              <p>
-                We are only dealing with{' '}
-                <span className={'bold'}>neck, shoulder</span> and{' '}
-                <span className={'bold'}>back </span>pain right now. We will
-                continue to update the library to include all the other areas.
-              </p>
-              <p className={'bold'}>It’s free to use.</p>
+              <ul>
+                <li>Use deep stroking massage, not static pressure.</li>
+                <li>Massage with short and slow repeated strokes.</li>
+                <li>Do the massage stroke in one direction only.</li>
+                <li>Aim at a pain level of 7 on a scale of one to 10.</li>
+                <li>No more than a 1 minute per trigger point. </li>
+                <li>
+                  Work on a trigger point 6 - 12 times per day if you can.
+                </li>
+              </ul>
             </div>
           </div>
           <div className={'infoBox'}>
@@ -75,19 +75,35 @@ export const Therapy: React.FC = () => {
               <p>General Tips</p>
             </div>
             <div className={'description'}>
-              <ul>
-                <li>
-                  Tell us where you are hurting by clicking on the corresponding
-                  spots on the anatomical model
-                </li>
-                <li>
-                  We will show you a list of techniques focused on addressing
-                  that area.
-                </li>
-                <li>
-                  Follow the animated gifs to rid of yourself of these pains.
-                </li>
-              </ul>
+              <p>
+                Move the skin with the fingers. Release at the end of the
+                stroke, then reset your fingers and repeat. You can massage with
+                the grain of the fibers or across them.
+              </p>
+              <p className={'bold'}>Hand technique suggestions</p>
+              <div className={cls.handTechniquesContainer}>
+                <div className={cls.handTechnique}>
+                  <img
+                    src={SupportedThumbTech}
+                    alt='img of supported thumb message technique'
+                  />
+                  <p>Supported Thumb</p>
+                </div>
+                <div className={cls.handTechnique}>
+                  <img
+                    src={SupportedFingerTech}
+                    alt='img of supported finger message technique'
+                  />
+                  <p>Supported Finger</p>
+                </div>
+                <div className={cls.handTechnique}>
+                  <img
+                    src={SupportedHandTech}
+                    alt='img of supported hand message technique'
+                  />
+                  <p>Supported Hand</p>
+                </div>
+              </div>
             </div>
           </div>
         </SideDescription>
