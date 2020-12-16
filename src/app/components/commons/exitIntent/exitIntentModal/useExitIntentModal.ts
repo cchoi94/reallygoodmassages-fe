fb38be493bd5b2892d142ed3f6f23249e10d795b
Cody Choi <cchoi_2294@hotmail.com>
2020-12-15 20:33:50 -0500

    changed exit intent modal to pop up every 8 minutes

--- src/app/components/commons/exitIntent/exitIntentModal/useExitIntentModal.ts
+++ src/app/components/commons/exitIntent/exitIntentModal/useExitIntentModal.ts
@@ -3,7 +3,7 @@ import { ExitIntent } from '../exitIntentLogic';
 import * as Cookie from 'js-cookie';
 
 const exitIntentModalSeenCookieString = 'exit_intent_modal_seen';
-const inFifteenMinutes = new Date(new Date().getTime() + 5 * 60 * 1000);
+const inEightMinutes = new Date(new Date().getTime() + 8 * 60 * 1000);
 
 export const useExitIntent = () => {
   const [showModal, setShowModal] = useState(false);
@@ -29,7 +29,7 @@ export const useExitIntent = () => {
   useEffect(() => {
     if (showModal) {
       Cookie.set(exitIntentModalSeenCookieString, 'true', {
-        expires: inFifteenMinutes,
+        expires: inEightMinutes,
       });
     }
   }, [showModal]);
